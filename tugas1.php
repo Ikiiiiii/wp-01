5df8119c1299a1e30c24ee67423ae94f6b8f9be4
Rizky AF <115469658+Ikiiiiii@users.noreply.github.com>
2025-04-12 16:44:31 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Produk</title>
+</head>
+<body>
+    <center>
+        <table border="1" cellspacing="2" cellpadding="10">
+            <tr>
+                <th colspan="3" bgcolor="orange">DAFTAR PRODUK</th>
+            </tr>
+            <tr>
+                <td rowspan="3" bgcolor="yellow"><img src="/webprogram/gitar.jpg" alt=""></td>
+                <td bgcolor="green">Nama <br> Produk</td>
+                <td bgcolor="blue">Schecter Synyster Custom</td>
+            </tr>
+            <tr>
+                <td bgcolor="green">Harga</td>
+                <td bgcolor="blue">Rp 21,816,705</td>
+            </tr>
+            <tr>
+                <td bgcolor="green">Fitur <br> Produk</td>
+                <td bgcolor="blue">
+                    <ul>
+                        <li>Guitar Colors Gloss Black with Silver Pinstripes</li>
+                        <li>Hardware Color Black</li>
+                        <li>Top Contour Flat Top</li>
+                        <li>Dexterity Right Handed</li>
+                        <li>Construction Set-Neck w/Ultra Access</li>
+                        <li>Body Material Mahogany</li>
+                        <li>Top Material Pinstripe</li>
+                        <li>Binding Black 1-ply</li>
+                        <li>Bridge Floyd Rose 1500 Series</li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+    </center>
+</body>
+</html>
